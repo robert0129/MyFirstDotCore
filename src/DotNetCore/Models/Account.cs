7be9121c95110a9af2a0fcdf73fbea5722382243
Robert <chaolong_chen@hotmail.com>
2017-01-18 23:27:58 +0800

    Add Account Class in Model

---
+++ src/DotNetCore/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore.Models
+{
+    public class Account
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string password { get; set; }
+    }
+}
