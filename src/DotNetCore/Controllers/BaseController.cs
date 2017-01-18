d7ba327de45a46089b21d6f025809cc20cd73067
Robert <chaolong_chen@hotmail.com>
2017-01-18 21:24:49 +0800

    Add BaseController to Inherits Controller

---
+++ src/DotNetCore/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Server.Kestrel.Internal.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace DotNetCore.Controllers
+{
+    public class BaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+    }
+}
--- src/DotNetCore/Controllers/ValuesController.cs
+++ src/DotNetCore/Controllers/ValuesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DotNetCore.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class ValuesController : BaseController
     {
         // GET api/values
         [HttpGet]
