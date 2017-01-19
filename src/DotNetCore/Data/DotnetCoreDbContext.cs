331dcda6d12dd11aaf860053a8a51c46f6b2c871
Robert <chaolong_chen@hotmail.com>
2017-01-19 23:36:02 +0800

    Add DbSet

--- src/DotNetCore/Data/DotnetCoreDbContext.cs
+++ src/DotNetCore/Data/DotnetCoreDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DotNetCore.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,9 @@ namespace DotNetCore.Data
         //}
         public DotnetCoreDbContext(DbContextOptions<DotnetCoreDbContext> options) : base(options)
         {
-            
+
         }
+
+        public DbSet<Account> Account {get;set;}
     }
 }
